f7eec72f1fe55259544f791065317ac68e7987bc
numuminov <66655438+numuminov@users.noreply.github.com>
2020-08-01 15:50:30 -0400

    Add files via upload

---
+++ HappyAgain.java
@@ -0,0 +1,26 @@
+public class HappyAgain {
+    public static void main(String[] args) {
+        System.out.println("HI");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //i added lines
+        int r=89;
+
+
+    }
+}
---
+++ HappyB20.java
@@ -0,0 +1,13 @@
+
+
+public class HappyB20 {
+    public static void main(String[] args) {
+        System.out.println("Happy Saturday");
+        System.out.println("goal");
+        int t =89;
+        String l="git";
+
+
+        //i did some changes
+    }
+}
